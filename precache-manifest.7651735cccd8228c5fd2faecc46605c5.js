32de1692630b094ac785026386fcff09122866f7
Drew11 <drew.naumenko@gmail.com>
2020-05-29 19:37:02 +0300

    Updates

---
+++ precache-manifest.7651735cccd8228c5fd2faecc46605c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281a6ad4e64ee63b07acaaf5feb3ffe7",
+    "url": "/redlab-test/index.html"
+  },
+  {
+    "revision": "e5a2e23e96589a914603",
+    "url": "/redlab-test/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "ebb09060d56b6dc7f9fa",
+    "url": "/redlab-test/static/js/2.a32e2bbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redlab-test/static/js/2.a32e2bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a2e23e96589a914603",
+    "url": "/redlab-test/static/js/main.6893e7c9.chunk.js"
+  },
+  {
+    "revision": "ad3f0cbe184afcf74236",
+    "url": "/redlab-test/static/js/runtime-main.c787ab72.js"
+  }
+]);
